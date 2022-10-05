596bc9cf4e9463a121f2ac57dbc7d3b2775ab9bd
jwzzang12 <moonbb12@gmail.com>
2022-10-05 21:08:59 +0900

    Set basic structure

---
+++ src/components/Preview.js
@@ -0,0 +1,3 @@
+export default function Preview() {
+  return <span class="material-icons-outlined">visibility</span>;
+}
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <div id="titleBox">
+      <input type="text" placeholder="제목 없는 설문지" id="title" />
+      <input type="text" placeholder="설문지 설명" id="titleDesc" />
+    </div>
+  );
+}
