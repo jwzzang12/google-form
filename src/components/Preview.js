2f9a72cab19535e957dfe8e5a686336e5c50c3df
jwzzang12 <moonbb12@gmail.com>
2022-10-10 22:54:32 +0900

    End

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,6 +1,9 @@
 export default function Preview() {
+  const onClickAlert = () => {
+    window.alert("Not available.");
+  };
   return (
-    <span className="material-icons" id="preview">
+    <span className="material-icons" id="preview" onClick={onClickAlert}>
       visibility
     </span>
   );
